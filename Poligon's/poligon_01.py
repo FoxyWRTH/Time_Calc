916f8375c13fe163fca0111b6b137ecac0d86cff
FoxyWRTH <f0xxxywtf@gmail.com>
2024-07-25 23:01:17 +0500

    Some_changes

--- Poligon's/poligon_01.py
+++ Poligon's/poligon_01.py
@@ -1,3 +1,15 @@
 """
 Полигон №1
 """
+from datetime import timedelta
+
+
+def calculate_time(hours_1, minutes_1, hours_2, minutes_2, operator):
+    first_window = timedelta(hours=hours_1, minutes=minutes_1)
+    second_window = timedelta(hours=hours_2, minutes=minutes_2)
+    operator = operator
+    return first_window - second_window
+
+
+if __name__ == '__main__':
+    print(calculate_time(10, 20, 12, 00, '-'))
